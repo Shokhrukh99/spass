fcc675b49bc20ab87585a41513dd3dfb4d1d89f7
Shokrukh <sultanovshoh@gmail.com>
2025-11-21 00:21:20 +0500

    [TW_10] Databases and deployment

--- spass/spass/settings.py
+++ spass/spass/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d_fn83tpm28097d1g+2lcm#k1y&^bbkhu^0rutxq5=dprb*w^*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -75,12 +75,17 @@ WSGI_APPLICATION = 'spass.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
+
 
 
 # Password validation
